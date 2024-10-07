e20aa4f7e57af6431f129856fc201ff741cec924
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-10-07 15:54:14 +0200

    Implement client-side storage Fixes #4

--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,34 @@
 import { View, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import React from 'react';
-import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import { Bubble, GiftedChat, InputToolbar } from 'react-native-gifted-chat';
 import { collection, addDoc, onSnapshot, query, where, orderBy, Timestamp } from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Chat({ route, navigation, database }) {
+export default function Chat({ route, navigation, database, isConnected }) {
     const name = route.params.name;
     const backgroundColor = route.params.selectedColor;
     const uid = route.params.userID;
     const [messages, setMessages] = React.useState([]);
     const onSend = (newMessages) => addDoc(collection(database, 'messages'), newMessages[0]);
     React.useEffect(() => {
-        const unsubscribeMessages = onSnapshot(query(collection(database, 'messages'), orderBy("createdAt", "desc")), messagesSnapshot => {
-            let newMessages = [];
-            messagesSnapshot.forEach(message => {
-                const messageData = message.data();
-                messageData.createdAt = new Timestamp(messageData.createdAt.seconds, messageData.createdAt.nanoseconds).toDate();
-                newMessages.push({ id: message.id, ...messageData });
+        if (isConnected) {
+            const unsubscribeMessages = onSnapshot(query(collection(database, 'messages'), orderBy("createdAt", "desc")), messagesSnapshot => {
+                let newMessages = [];
+                messagesSnapshot.forEach(message => {
+                    const messageData = message.data();
+                    messageData.createdAt = new Timestamp(messageData.createdAt.seconds, messageData.createdAt.nanoseconds).toDate();
+                    newMessages.push({ id: message.id, ...messageData });
+                });
+                setMessages(newMessages);
+                AsyncStorage.setItem('messages', JSON.stringify(newMessages))
+                    .catch(error => console.error('Error saving messages to AsyncStorage', error));
+                return () => { if (unsubscribeMessages) unsubscribeMessages() };
             });
-            setMessages(newMessages);
-            return () => { if (unsubscribeMessages) unsubscribeMessages() };
-        });
+        } else {
+            AsyncStorage.getItem('messages').then(messages => {
+                if (messages) setMessages(JSON.parse(messages));
+            }).catch(error => console.error('Error retrieving messages from AsyncStorage', error));
+        }
     }, []);
     React.useEffect(() => {
         if (name && navigation) navigation.setOptions({
@@ -27,26 +36,38 @@ export default function Chat({ route, navigation, database }) {
             backgroundColor: backgroundColor
         });
     }, [navigation, name, backgroundColor]);
+
+    const renderInputToolbar = (props) => {
+        return isConnected ? <InputToolbar {...props} /> : null;
+    };
+
+    const renderBubble = (props) => {
+        return (
+            <Bubble {...props} wrapperStyle={{
+                right: {
+                    backgroundColor: '#000'
+                },
+                left: {
+                    backgroundColor: '#fff'
+                }
+            }} />
+        );
+    };
+
     return (
         <View style={[styles.container, { backgroundColor }]}>
-            <GiftedChat messages={messages} renderBubble={renderBubble} onSend={onSend} user={{ _id: uid, name: name }} />
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                renderInputToolbar={renderInputToolbar}
+                onSend={onSend}
+                user={{ _id: uid, name: name }}
+            />
             {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null}
         </View>
     );
 }
 
-const renderBubble = (props) => {
-    return (
-        <Bubble {...props} wrapperStyle={{
-            right: {
-                backgroundColor: '#000'
-            },
-            left: {
-                backgroundColor: '#fff'
-            }
-        }} />
-    );
-};
 
 const styles = StyleSheet.create({
     container: {
