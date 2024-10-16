9c64ef67b57d8090c7b8b149e966660b4a45388b
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-10-16 13:03:29 +0200

    Refactor Chat component: add JSDoc comments for better type definitions and improve code readability

--- components/Chat.js
+++ components/Chat.js
@@ -1,80 +1,73 @@
 import { View, KeyboardAvoidingView, Platform, TouchableOpacity, Image } from 'react-native';
 import React, { useEffect } from 'react';
 import { Bubble, GiftedChat, InputToolbar } from 'react-native-gifted-chat';
-import { collection, addDoc, onSnapshot, query, orderBy, Timestamp } from "firebase/firestore";
+import { collection, addDoc, onSnapshot, query, orderBy, Timestamp, Firestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import MapView from 'react-native-maps';
 import CustomActions from './CustomActions';
 
+/**
+ * @typedef {Object} ChatRouteParams
+ * @property {String} name
+ * @property {String} selectedColor
+ * @property {String} userID
+ */
+
+/**
+ * @typedef {import('@react-navigation/native').RouteProp<{ Chat: ChatRouteParams }, 'Chat'>} ChatRouteProp
+ */
+
+/**
+ * @typedef {import('firebase/storage').FirebaseStorage} Storage
+ */
+
+/**
+ * @typedef {import('react-native-gifted-chat').IMessage} Message
+ */
+
+/**
+ * @typedef {import('react-native-gifted-chat').BubbleProps<Message & CustomMessage>} BubbleProps
+ */
+
+/**
+ * @typedef {Object} CustomMessage
+ * @property {Location} location?
+ * @property {string} imageURL?
+ */
+
+/**
+ * @typedef {Object} Location
+ * @property {number} latitude
+ * @property {number} longitude
+ */
+
+/**
+ * @typedef {Object} ChatProps
+ * @property {ChatRouteProp} route
+ * @property {Object} navigation
+ * @property {Firestore} database
+ * @property {Storage} storage
+ * @property {Boolean} isConnected
+ */
+
+/**
+ * @function Chat
+ * @param {ChatProps} configuration 
+ * @returns {React.JSX.Element}
+ */
 export default function Chat({ route, navigation, database, storage, isConnected }) {
-    const { name, selectedColor: backgroundColor, userID } = route?.params,
-        [messages, setMessages] = React.useState([]),
+    /**
+     * Extracts the name, selectedColor as backgroundColor and userID from the route params
+     * 
+     * @type {ChatRouteParams}
+     */
+    const { name, selectedColor: backgroundColor, userID } = route?.params;
 
-        onSend = (newMessages) => {
-            addDoc(collection(database, 'messages'), newMessages[0]);
-        },
-
-        renderInputToolbar = (props) => {
-            return isConnected ? <InputToolbar {...props} /> : null;
-        },
-
-        renderBubble = (props) => {
-            return (
-                <Bubble {...props} wrapperStyle={{
-                    right: {
-                        backgroundColor: '#000' // TODO #14
-                    },
-                    left: {
-                        backgroundColor: '#fff' // TODO #14
-                    }
-                }} />
-            );
-        },
-
-        renderCustomActions = (props) => {
-            let messageId;
-            do {
-                messageId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                    const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-                    return v.toString(16);
-                });
-            } while (messages.some(message => message._id === messageId)); // find a unique UUID
-            return <CustomActions
-                onSend={onSend}
-                user={
-                    {
-                        _id: userID,
-                        name: name,
-                        avatar: 'https://placeimg.com/140/140/any' // TODO #13
-                    }
-                }
-                messageId={messageId}
-                storage={storage}
-                {...props}
-            />;
-        },
-
-        renderCustomView = (props) => {
-            const { location, imageURL } = props?.currentMessage;
-            if (location) {
-                return (
-                    <MapView style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }} region={{
-                        latitude: location.latitude,
-                        longitude: location.longitude,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421
-                    }} />
-                );
-            }
-            if (imageURL) { // The gifted chat image viewer is not working, so I decided to use a custom image viewer
-                return (
-                    <TouchableOpacity onPress={() => navigation.navigate('ImageView', { imageURL: imageURL })}>
-                        <Image source={{ uri: imageURL }} style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }} />
-                    </TouchableOpacity>
-                );
-            }
-        };
-
+    /**
+     * A state hook to  store chat messages
+     * @type {[Message[], React.Dispatch<React.SetStateAction<Message[]>>]}
+     */
+    const [messages, setMessages] = React.useState([]);
 
     useEffect(() => {
         if (isConnected) {
@@ -104,7 +97,8 @@ export default function Chat({ route, navigation, database, storage, isConnected
                 _id: 'system-message',
                 text: 'You are offline, please check your network connection.',
                 createdAt: new Date(),
-                system: true
+                system: true,
+                user: { _id: 0 }
             };
             setMessages(previousMessages => GiftedChat.append(previousMessages, [systemMessage]));
         }
@@ -115,6 +109,97 @@ export default function Chat({ route, navigation, database, storage, isConnected
             title: name
         });
     }, [navigation, name]);
+
+    /**
+     * 
+     * @param {Readonly<BubbleProps>} props 
+     * @returns {React.JSX.Element}
+     */
+    function renderBubble(props) {
+        return (
+            <Bubble {...props} wrapperStyle={{
+                right: {
+                    backgroundColor: '#000' // TODO #14
+                },
+                left: {
+                    backgroundColor: '#fff' // TODO #14
+                }
+            }} />
+        );
+    }
+
+    /**
+     * 
+     * @param {import('react-native-gifted-chat').InputToolbarProps<import('react-native-gifted-chat').IMessage>} props 
+     * @returns {React.JSX.Element | null}
+     */
+    function renderInputToolbar(props) {
+        return isConnected ? <InputToolbar {...props} /> : null;
+    }
+
+    /**
+     * @function onSend
+     * @param {Message[]} newMessages
+     * @returns {void}
+     */
+    function onSend(newMessages) {
+        addDoc(collection(database, 'messages'), newMessages[0]);
+    }
+
+    /**
+     * 
+     * @param {import('react-native-gifted-chat').ActionsProps} props
+     * @returns {React.JSX.Element}
+     */
+    function renderCustomActions({ wrapperStyle, iconTextStyle }) {
+        let messageId;
+        do {
+            messageId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        } while (messages.some(message => message._id === messageId)); // find a unique UUID
+        return <CustomActions
+            onSend={onSend}
+            user={
+                {
+                    _id: userID,
+                    name: name,
+                    avatar: 'https://placeimg.com/140/140/any' // TODO #13
+                }
+            }
+            messageId={messageId}
+            storage={storage}
+            wrapperStyle={wrapperStyle}
+            iconTextStyle={iconTextStyle}
+        />;
+    }
+
+    /**
+     * 
+     * @param {BubbleProps} props 
+     * @returns {React.JSX.Element}
+     */
+    function renderCustomView(props) {
+        const { location, imageURL } = props?.currentMessage;
+        if (location) {
+            return (
+                <MapView style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }} region={{
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421
+                }} />
+            );
+        }
+        if (imageURL) { // The gifted chat image viewer is not working, so I decided to use a custom image viewer
+            return (
+                <TouchableOpacity onPress={() => navigation.navigate('ImageView', { imageURL: imageURL })}>
+                    <Image source={{ uri: imageURL }} style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }} />
+                </TouchableOpacity>
+            );
+        }
+    }
 
     return (
         <View style={{ flex: 1, backgroundColor }}>
